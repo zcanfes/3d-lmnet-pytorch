a872b44b517556db85badd32897158c033888952
Zehranaz Canfes <zcanfes@gmail.com>
2022-12-26 13:20:54 +0100

    Fix format

--- code/training/train_2d_to_pointcloud_matching.py
+++ code/training/train_2d_to_pointcloud_matching.py
@@ -15,7 +15,9 @@ from data.shapenet import ShapeNet
 import os
 
 
-def train(model_image, model_pointcloud, train_dataloader, valid_dataloader, device, config):
+def train(
+    model_image, model_pointcloud, train_dataloader, valid_dataloader, device, config
+):
 
     loss = None
     if config["loss_criterion"] == "variational":
