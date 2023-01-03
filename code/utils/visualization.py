4593d90d7091f2f6f11d071f7df8f9d0e15049ff
GozdeUnver <56361766+GozdeUnver@users.noreply.github.com>
2023-01-03 09:49:30 +0100

    Update visualization.py

--- code/utils/visualization.py
+++ code/utils/visualization.py
@@ -1,2 +1,18 @@
+import k3d
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
 def visualize_pointcloud(points):
-    pass
+    try:
+        points=points.cpu().numpy()
+    catch:
+        continue
+    plt_points = k3d.points(positions=points.astype(np.float32) ,point_size=0.2, shader='3d',color=0x3f6bc5)
+    plot = k3d.plot(grid=(-0.55, -0.55, -0.55, 0.55, 0.55, 0.55))
+    plot += plt_points
+    plot.display()
+                        
+
+def visualize_image(img):
+    plt.imshow(img)
