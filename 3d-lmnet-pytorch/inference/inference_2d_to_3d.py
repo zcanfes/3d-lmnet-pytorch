97093736d137862b6101e1adc1d1b7a1411b08b3
GozdeUnver <56361766+GozdeUnver@users.noreply.github.com>
2023-01-18 22:19:48 +0100

    Update inference_2d_to_3d.py

--- 3d-lmnet-pytorch/inference/inference_2d_to_3d.py
+++ 3d-lmnet-pytorch/inference/inference_2d_to_3d.py
@@ -15,7 +15,7 @@ from utils.losses import DiversityLoss, SquaredEuclideanError, LeastAbsoluteErro
 import os
 from data.shapenet import ShapeNet
 
-def test(encoder, autoencoder, test_dataloader, device, config):
+def test(encoder, autoencoder, test_dataloader, device, config,len_test_dataset):
     if config["loss_criterion"] == "variational":
 
         # TODO: DiversityLoss TANIMLA !!!!!!!
@@ -38,8 +38,7 @@ def test(encoder, autoencoder, test_dataloader, device, config):
     total_test_loss=0.
     index=-1
     for i, batch in enumerate(test_dataloader):
-        # Move batch to device
-        print("Batch",i)
+        
         index+=1
         
         with torch.no_grad():
@@ -51,35 +50,33 @@ def test(encoder, autoencoder, test_dataloader, device, config):
             
             if config["loss_criterion"] == "variational":
                 
-                mu, log_var = encoder(batch["img"][12][:,:,:128,:128])
+                mu, log_var = encoder(batch["img"])
                 std = torch.sqrt(torch.exp(log_var))
                 pred_latent = mu + torch.randn(std.size()) * std
                 pred_pointcloud = autoencoder.decoder(pred_latent)
-                batch_min=1e3
+                
                 for j in range(len(pred_pointcloud)):
                     loss,_=chamfer_distance(batch["point"].permute(0, 2, 1), pred_pointcloud[j].permute(0, 2, 1))
                     
                     distance = loss.detach().cpu()
-                    if distance<batch_min:
-                        batch_min=distance
                     
-                    print("Chamfer distance for prediciton",j,":",distance)
+                    
+                    print("Chamfer distance for test data:",i," the",j,"th prediciton is :",distance)
                     with open(config["2d_inference_variational"] + str(index)+"_"+str(j) + ".npy", "wb") as f:
-                        np.save(f, pred_pointcloud[i])
+                        np.save(f, pred_pointcloud[j])
 
-                total_test_loss+=batch_min
+                total_test_loss+=distance
             else:
-                pred_pointcloud = decoder(encoder(batch["img"][12][:,:,:128,:128]))
+                pred_pointcloud = autoencoder.decoder(encoder(batch["img"])
                 loss,_=chamfer_distance(batch["point"].permute(0, 2, 1), pred_pointcloud.permute(0, 2, 1))
                 distance = loss.detach().cpu()
-                print("Chamfer distance for batch",i,":",distance)
+                print("Chamfer distance for test input",i,":",distance)
                 with open(config["2d_inference_normal"] + str(index) + ".npy", "wb") as f:
                     np.save(f, pred_pointcloud)
 
                 total_test_loss+=distance
 
-                print("Chamfer loss value for prediction:", distance)
-    print("Total test chamfer distance:", total_test_loss)
+    print("Total test chamfer distance:", total_test_loss/len_test_dataset)
 
 def main(config):
     device = torch.device("cpu")
@@ -93,7 +90,7 @@ def main(config):
     test_dataloader = torch.utils.data.DataLoader(
         test_dataset, batch_size=config["batch_size"], shuffle=False, num_workers=2
     )
-
+    len_test_dataset=len(test_dataset)
     encoder = ImageEncoder(config["final_layer"], config["bottleneck"])
     encoder = encoder.load_state_dict(
         torch.load(config["encoder_path"], map_location="cpu")
@@ -113,5 +110,6 @@ def main(config):
         test_dataloader,
         device,
         config,
+        len_test_dataset
     )
 
