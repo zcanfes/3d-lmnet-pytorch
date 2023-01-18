50cc3eccee173b926011bd9ae29cbac33bc05ca4
GozdeUnver <56361766+GozdeUnver@users.noreply.github.com>
2023-01-18 21:08:51 +0100

    Update shapenet.py

--- 3d-lmnet-pytorch/data/shapenet.py
+++ 3d-lmnet-pytorch/data/shapenet.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 import os
 import cv2
-
+from itertools import product
 
 class ShapeNet(torch.utils.data.Dataset):
     num_classes = 13
@@ -18,47 +18,68 @@ class ShapeNet(torch.utils.data.Dataset):
     class_to_nums_mapping=json.loads(Path("/content/3d-lmnet-pytorch/3d-lmnet-pytorch/data/class_to_nums.json").read_text())
     class_nums = sorted(num_to_class_mapping.keys())
 
-    def __init__(self, split, cat=13):
+    def __init__(self, split, cat=13,image_model=False):
         super().__init__()
         assert split in ["train", "valid", "test"]
         # self.truncation_distance = 3
+        self.image_model=image_model
         if cat == 3:
-            self.point_items = (
+            temp = (
                 Path(
                     f"/content/3d-lmnet-pytorch/3d-lmnet-pytorch/data/splits/shapenet_point_cat3/{split}_cat3.txt"
                 )
                 .read_text()
                 .splitlines()
             )  # keep track of shapes based on split
+            if self.image_model:
+                idx=np.arange(24)
+                self.point_items=list(product(temp,idx))
+            else:
+                self.point_items=temp
 
         elif cat == 1:
-            self.point_items = (
+            temp = (
                 Path(
                     f"/content/3d-lmnet-pytorch/3d-lmnet-pytorch/data/splits/shapenet_point_chair/{split}_chair.txt"
                 )
                 .read_text()
                 .splitlines()
             )  # keep track of shapes based on split
-
+            if self.image_model:
+                idx=np.arange(24)
+                self.point_items=list(product(temp,idx))
+            else:
+                self.point_items=temp
         else:
-            self.point_items = (
+            temp = (
                 Path(f"/content/3d-lmnet-pytorch/3d-lmnet-pytorch/data/splits/shapenet_point/{split}.txt")
                 .read_text()
                 .splitlines()
             )  # keep track of shapes based on split
+            if self.image_model:
+                idx=np.arange(24)
+                self.point_items=list(product(temp,idx))
+            else:
+                self.point_items=temp
 
         self.img_items = self.point_items
 
     def __getitem__(self, index):
-        img_index = self.img_items[index]
-        point_index = self.point_items[index]
-
-        img,azimuth = ShapeNet.get_img_and_azimuth(img_index)
-        point = ShapeNet.get_point_numpy(point_index)
-
+        
+        if not self.image_model:
+            img_path= self.img_items[index]
+            point_path = self.point_items[index]
+            img,azimuth = ShapeNet.get_img_and_azimuth(img_path)
+            point = ShapeNet.get_point_numpy(point_path)
+        else:
+            img_path,img_index = self.img_items[index]
+            point_path,_ = self.point_items[index]
+            img,azimuth = ShapeNet.get_img_and_azimuth(img_path,img_index)
+            point = ShapeNet.get_point_numpy(point_path)
         return {"img": img, "point": point,"azimuth":azimuth}
 
     def __len__(self):
+        
         return len(self.point_items)
 
     @staticmethod
@@ -77,13 +98,13 @@ class ShapeNet(torch.utils.data.Dataset):
         return convertedArray
 
     @staticmethod
-    def get_img_and_azimuth(shapenet_id):
+    def get_img_and_azimuth(shapenet_id,img_index=0):
         np_res = []
         np_azimuth=[]
         meta_file=str(ShapeNet.img_path / shapenet_id / "rendering" / "rendering_metadata.txt")
         
         meta=np.loadtxt(meta_file)
-        for _, _, files in os.walk(ShapeNet.img_path / shapenet_id):
+        """for _, _, files in os.walk(ShapeNet.img_path / shapenet_id):
             png_files=[int(f[:-4]) for f in files if f[-3:] == "png"]
             
             png_files.sort()
@@ -95,7 +116,13 @@ class ShapeNet(torch.utils.data.Dataset):
                 image = np.array(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
                 
                 np_res.append(image)
-                np_azimuth.append((np.pi / 180.)*meta[i][0])
-        return np.transpose(np.array(np_res),(0,3,1,2)),np.array(np_azimuth)
+                np_azimuth.append((np.pi / 180.)*meta[i][0])"""
+                
+        file_name=str(img_index).rjust(2, "0")+".png"
+        p = str(ShapeNet.img_path / shapenet_id / "rendering" / file_name)
+        image = cv2.imread(p)[4:-5, 4:-5, :3]
+        image = np.array(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        azimuth=(np.pi / 180.)*meta[img_index][0]
+        return np.transpose(image,(2,0,1)),np.array(azimuth)
 
     
