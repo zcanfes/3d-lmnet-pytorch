f23542eacc6d71c99f52c5ecb9600b04d9132ac1
Zehranaz Canfes <zcanfes@gmail.com>
2023-01-19 14:33:34 +0100

    Delete custom chamferloss

--- 3d-lmnet-pytorch/utils/losses.py
+++ 3d-lmnet-pytorch/utils/losses.py
@@ -14,33 +14,3 @@ class DiversityLoss(nn.Module):
         penalty_angle = np.pi / 180.0 * penalty_angle
 
         return alpha * np.exp(-((azimuth - np.pi) ** 2 / (penalty_angle**2)))
-
-
-
-
-class ChamferLoss(torch.autograd.Function):
-    """
-    Calculate chamfer, forward, backward distance between ground truth and predicted
-    point clouds. They may or may not be scaled.
-    """
-
-    @staticmethod
-    def forward(ctx, y_true, y_pred):
-        ctx.save_for_backward(y_true, y_pred)
-
-        dists_forward = torch.cdist(y_true, y_pred, p=2)
-        dists_forward = torch.sqrt(dists_forward).mean()
-
-        # print("dists forward shape:", dists_forward)
-
-        chamfer_distance = 2 * dists_forward
-        return chamfer_distance
-    
-    @staticmethod
-    def backward(ctx):
-        yy_true, yy_pred = ctx.saved_tensors
-
-        dists_backward = torch.cdist(yy_true, yy_pred, p=2)
-        dists_backward = torch.sqrt(dists_backward).mean()
-
-        return dists_backward
