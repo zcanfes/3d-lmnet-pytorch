fef35688c78a5c4a90e121108b139359db3666e7
GozdeUnver <56361766+GozdeUnver@users.noreply.github.com>
2023-01-20 15:09:58 +0100

    Update inference_2d_to_3d.py

--- 3d-lmnet-pytorch/inference/inference_2d_to_3d.py
+++ 3d-lmnet-pytorch/inference/inference_2d_to_3d.py
@@ -37,41 +37,57 @@ def test(encoder, autoencoder, test_dataloader, device, config,len_test_dataset)
     total_test_loss=0.
     index=-1
     for i, batch in enumerate(test_dataloader):
-        
+        if i==200:
+            break
         index+=1
         
         with torch.no_grad():
             ShapeNet.move_batch_to_device(batch, device)
-            with open(config["2d_inference_gt_point"] + str(index)+ ".npy", "wb") as f:
-                np.save(f, batch["point"].permute(0, 2, 1))
-            with open(config["2d_inference_gt_img"] + str(index)+ ".npy", "wb") as f:
-                np.save(f, batch["img"])
+            point_clouds = batch["point"]
+            
+            # print("point_clouds shape:", point_clouds.shape)
+            point_clouds = point_clouds.permute(0, 2, 1)
+            point_clouds=point_clouds.type(torch.cuda.FloatTensor)
+
+            image=batch["img"]
+            
+            image=image.type(torch.cuda.FloatTensor)
+
+            with open(config["2d_inference_gt_point"] + "pointcloud_"+str(index)+ ".npy", "wb") as f:
+                np.save(f, point_clouds.permute(0, 2, 1).cpu().numpy())
+            with open(config["2d_inference_gt_img"] + "image_"+str(index)+ ".npy", "wb") as f:
+                np.save(f, image.cpu().numpy())
             
             if config["loss_criterion"] == "variational":
                 
-                mu, log_var = encoder(batch["img"])
+                mu, log_var = encoder(image)
                 std = torch.sqrt(torch.exp(log_var))
                 pred_latent = mu + torch.randn(std.size()) * std
                 pred_pointcloud = autoencoder.decoder(pred_latent)
                 
                 for j in range(len(pred_pointcloud)):
-                    loss,_=chamfer_distance(batch["point"].permute(0, 2, 1), pred_pointcloud[j].permute(0, 2, 1))
+                    loss,_=chamfer_distance(point_clouds, pred_pointcloud[j])
                     
                     distance = loss.detach().cpu()
                     
                     
                     print("Chamfer distance for test data:",i," the",j,"th prediciton is :",distance)
-                    with open(config["2d_inference_variational"] + str(index)+"_"+str(j) + ".npy", "wb") as f:
-                        np.save(f, pred_pointcloud[j])
+                    with open(config["2d_inference_variational"] + "inference_"+str(index)+"_"+str(j) + ".npy", "wb") as f:
+                        np.save(f, pred_pointcloud[j].cpu().numpy())
 
                 total_test_loss+=distance
             else:
-                pred_pointcloud = autoencoder.decoder(encoder(batch["img"]))
-                loss,_=chamfer_distance(batch["point"].permute(0, 2, 1), pred_pointcloud.permute(0, 2, 1))
+                enc_output=encoder(image)
+                
+                pred_pointcloud = autoencoder.decoder(enc_output)
+                
+                #print(point_clouds.size(),pred_pointcloud.size())
+                loss,_=chamfer_distance(point_clouds, pred_pointcloud)
                 distance = loss.detach().cpu()
                 print("Chamfer distance for test input",i,":",distance)
-                with open(config["2d_inference_normal"] + str(index) + ".npy", "wb") as f:
-                    np.save(f, pred_pointcloud)
+                with open(config["2d_inference_normal"] + "inference_"+str(index) + ".npy", "wb") as f:
+                    
+                    np.save(f, pred_pointcloud.cpu().numpy())
 
                 total_test_loss+=distance
 
@@ -91,14 +107,14 @@ def main(config):
     )
     len_test_dataset=len(test_dataset)
     encoder = ImageEncoder(config["final_layer"], config["bottleneck"])
-    encoder = encoder.load_state_dict(
+    encoder.load_state_dict(
         torch.load(config["encoder_path"], map_location="cpu")
     )
     autoencoder=AutoEncoder(config["autoencoder_bottleneck"],config["autoencoder_hidden_size"],config["autoencoder_output_size"])
     
     autoencoder.load_state_dict(torch.load(config["3d_autoencoder_path"], map_location="cpu"))
     
-    
+    print("Length of test datsaset:",len_test_dataset)
     
     encoder.to(device)
     autoencoder.to(device)
