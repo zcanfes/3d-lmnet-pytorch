cf50bf29a57f96c6ad64cd6e9712d899885a9532
Zehranaz Canfes <zcanfes@gmail.com>
2023-01-25 14:30:53 +0100

    Fix diversity loss

--- 3d-lmnet-pytorch/utils/losses.py
+++ 3d-lmnet-pytorch/utils/losses.py
@@ -7,22 +7,14 @@ class DiversityLoss(nn.Module):
 
     def __call__(self, alpha, penalty_angle, azimuth_input, z_sigma):
         z_alpha = self.get_alpha(alpha, penalty_angle, azimuth_input)
-        #print("z_alpha")
-        L_reg = torch.mean((torch.mean(z_sigma, dim=-1) - z_alpha.cuda()) ** 2)
+        # print("z_alpha: ", z_alpha.shape)
+        # print("z_sigma: ", z_sigma.shape)
+        # print("after mean: ", torch.mean((torch.mean(z_sigma, axis=-1) - z_alpha)**2))
+        L_reg = torch.mean((torch.mean(z_sigma, axis=-1) - z_alpha)**2)
         #print("l_reg")
         return L_reg
 
     def get_alpha(self, alpha, penalty_angle, azimuth):
-        # print("get alpha")
-        # penalty_angle = np.pi / 180.0 * penalty_angle
-        # print("penalty")
-        # expr= np.exp(-((azimuth - np.pi) ** 2))
-        # expr = expr.detach().cpu()
-        # print("expr")
-        # alpha_expr = (alpha* expr**2).cpu()
-        # print("alpha_expr")
-        # penalty_expr =  (penalty_angle**2).cpu()
-        # return (alpha_expr / penalty_expr)
-        penalty_angle = np.pi / 180.0 * penalty_angle
+        penalty_angle = (np.pi / 180.0) * penalty_angle
 
-        return alpha * np.exp(-((azimuth.cpu() - np.pi) ** 2 / (penalty_angle **2)))
+        return (alpha * np.exp(-((azimuth.cpu()-np.pi)**2/(penalty_angle**2)))).cuda()
