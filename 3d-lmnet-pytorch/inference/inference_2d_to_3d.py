08e7d45c51fc83d4ce601e1b7a510c8abffc7ee6
GozdeUnver <56361766+GozdeUnver@users.noreply.github.com>
2023-02-07 18:12:29 +0100

    removed latent loss calc from variant 2 inference, updated jupyter and inference code

--- 3d-lmnet-pytorch/inference/inference_2d_to_3d.py
+++ 3d-lmnet-pytorch/inference/inference_2d_to_3d.py
@@ -8,22 +8,12 @@ from model.model_2d import ImageEncoder
 from model.model_3d_autoencoder import AutoEncoder
 import os
 from data.shapenet import ShapeNet
-from utils.losses import DiversityLoss
 
 def test(encoder, autoencoder, test_dataloader, device, config):
-    if config["loss_criterion"]=="variational":
-        loss_div = DiversityLoss()
-        loss_latent_matching = nn.MSELoss()
-
-        loss_latent_matching.to(device)
-        loss_div.to(device)
-        ALPHA = config["alpha"]
-        PENALTY_ANGLE = config["penalty_angle"]
-        LAMBDA = config["lambda"]
-    elif config["loss_criterion"] == "L1":
+    if config["loss_criterion"] == "L1":
         loss_criterion = nn.L1Loss()
         loss_criterion.to(device)
-    else:
+    elif config["loss_criterion"] == "L2":
         loss_criterion = nn.MSELoss()
         loss_criterion.to(device)
 
@@ -57,23 +47,21 @@ def test(encoder, autoencoder, test_dataloader, device, config):
                 
                 mu, log_var = encoder(image)
                 std = torch.sqrt(torch.exp(log_var))
-                AZIMUTH_INPUT = batch["azimuth"]
-                AZIMUTH_INPUT=AZIMUTH_INPUT.type(torch.cuda.FloatTensor)
+                
                 pred_latent = mu + torch.randn((3,512),device=device) * std
                 pred_pointcloud = autoencoder.decoder(pred_latent) 
                 distance=0.
-                t=0.
+                
                 for j in range(3):
                     loss,_=chamfer_distance(point_clouds.permute(0,2,1), pred_pointcloud[j,None,:,:].permute(0,2,1))
                     
                     distance += loss.detach().cpu()
-                    loss_ = loss_latent_matching(pred_latent[j,None], autoencoder.encoder(point_clouds)) + LAMBDA * loss_div(ALPHA, PENALTY_ANGLE, AZIMUTH_INPUT, std)
-                    t+=loss_.item()
+                    
                     if i<50:
                         with open(config["2d_inference_pred"] + "inference_"+str(index)+"_"+str(j) + ".npy", "wb") as f:
                             np.save(f, pred_pointcloud[j,None,:,:].cpu().numpy())
                 total_test_loss+=distance/3
-                latent_loss+=t/3
+                
             else:
                 enc_output=encoder(image)
                 
@@ -91,11 +79,9 @@ def test(encoder, autoencoder, test_dataloader, device, config):
                 total_test_loss+=distance
 
     print("Total test chamfer distance:", total_test_loss/len(test_dataloader))
-    if config["loss_criterion"] == "variational":
-        print("Total test lambda * diversity + L2 loss:", latent_loss/len(test_dataloader))
-    elif config["loss_criterion"] == "L1":
+    if config["loss_criterion"] == "L1":
         print("Total test L1 loss:", latent_loss/len(test_dataloader))
-    else:
+    elif config["loss_criterion"] == "L2":
         print("Total test L2 loss:", latent_loss/len(test_dataloader))
 
 def main(config):
